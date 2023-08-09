dbad0d50bad466583d3db635be596859991ba144
Kenneycod <81203052+Kenneycod@users.noreply.github.com>
2023-08-09 08:42:17 +0300

    new commit

---
+++ database.js
@@ -0,0 +1,62 @@
+const { ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv').config();
+
+const uri = process.env.URI; 
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connect() {
+  await client.connect().then(()=>{
+    console.log('connected to mongodb......')
+  });
+}
+
+async function close() {
+  await client.close();
+}
+
+async function createDocument(dbName, collectionName, data) {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.insertOne(data);
+  return result;
+}
+
+async function readDocument(dbName, collectionName, filter) {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const documents = await collection.find(filter).toArray();
+  return documents;
+}
+
+async function updateDocument(dbName, collectionName, filter, update) {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.updateOne(filter, { $set: update });
+  return result;
+}
+
+async function deleteDocument(dbName, collectionName, filter) {
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+  const result = await collection.deleteOne(filter);
+  return result;
+}
+
+module.exports = {
+  connect,
+  close,
+  createDocument,
+  readDocument,
+  updateDocument,
+  deleteDocument,
+};
+
--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
+const dotenv = require('dotenv').config();
+const {connect, close,createDocument,readDocument,updateDocument,deleteDocument,}=require('./database');
 
 const app = express();
 app.use(express.static('public'));
@@ -9,8 +11,31 @@ app.use(bodyparser.urlencoded({extended:true}));
 //setting ejs engine
 app.set('view engine','ejs');
 
-app.get('/',(req,res)=>{
+app.get('/home',(req,res)=>{
     res.render('home')
+})
+
+app.get('/contact',(req,res)=>{
+    res.render('home')
+})
+
+app.post('/home',async (req,res)=>{
+    const {firstname,lastname,email,subject} = req.body;
+
+        try{
+            await connect();
+            const dbName = process.env.dbName;
+            const collectionName = process.env.collectionName;
+
+            const newData = {firstname,lastname,email,subject};
+            const createData = await createDocument(dbName,collectionName,newData);
+            console.log('created: ',createData);
+        }finally{
+            await close();
+        }
+
+    
+    res.redirect('/home')
 })
 
 app.listen(3000,()=>{
