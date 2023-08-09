9406aac34ec47c19f5b52b914083c6237889a57a
Kenneycod <81203052+Kenneycod@users.noreply.github.com>
2023-08-10 01:20:47 +0300

    new commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const ejs = require('ejs');
 const dotenv = require('dotenv').config();
+const nodemailer = require('nodemailer');
 const {connect, close,createDocument,readDocument,updateDocument,deleteDocument,}=require('./database');
 
 const app = express();
@@ -10,6 +11,19 @@ app.use(bodyparser.urlencoded({extended:true}));
 
 //setting ejs engine
 app.set('view engine','ejs');
+
+// configur mail sender
+const transporter = nodemailer.createTransport({
+    service: process.env.mailService,
+    host:process.env.host,
+    port:process.env.port,
+    secure:true,
+
+    auth: {
+        user:process.env.user,
+        pass:process.env.pass,
+    }
+})
 
 app.get('/home',(req,res)=>{
     res.render('home')
@@ -30,7 +44,23 @@ app.post('/home',async (req,res)=>{
             const newData = {firstname,lastname,email,subject};
             const createData = await createDocument(dbName,collectionName,newData);
             console.log('created: ',createData);
-        }finally{
+
+            const emailContent = {
+                from: process.env.user,
+                to:'chibkennedy@gmail.com',
+                subject: 'New Contact Form Submission',
+                text:subject,
+            }
+
+            transporter.sendMail(emailContent,(err,info)=>{
+                if (err){
+                    console.log(err)
+                } else {
+                    console.log('Email sent successfully: ',info)
+                }
+            })
+
+        } finally {
             await close();
         }
 
