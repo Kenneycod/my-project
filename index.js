71e324c96325d7efeae5f6b642a702735ef22d07
Kenneycod <81203052+Kenneycod@users.noreply.github.com>
2023-08-21 14:20:17 +0300

    new commit

--- index.js
+++ index.js
@@ -210,6 +210,6 @@ app.post('/home',async (req,res)=>{
     res.redirect('/home')
 })
 
-app.listen(3000,()=>{
-    console.log('server is running on port 3000');
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('server is running .........');
 })
